914d296139292c5abc588b7f8c0893957c149a81
Christian Anderson <canderson@cpp.edu>
2025-01-17 03:10:38 -0800

    starting the home page of the game.

---
+++ lib/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget{
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<Homepage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+
+    );
+  }
+}
