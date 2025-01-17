61229daa1cf9b33c041a2d829d199ed8fe5f8ea1
Christian Anderson <canderson@cpp.edu>
2025-01-17 03:13:58 -0800

    another fix

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,7 +5,7 @@ class HomePage extends StatefulWidget{
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<Homepage>{
+class _HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
