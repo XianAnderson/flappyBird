2f71ab91c1f4292ec27aad525d83a1ecbfcf8618
Christian Anderson <canderson@cpp.edu>
2025-01-17 03:04:20 -0800

    beginning changes to main

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,12 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
