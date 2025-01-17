bce689c86bc00395509898128a81326d44c112d4
Christian Anderson <canderson@cpp.edu>
2025-01-17 05:29:40 -0800

    starting to add mechanics to flappy bird to go up and down only while staying in the center of the screen.

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flappybirdie/bird.dart';
 import 'package:flutter/material.dart';
 
@@ -7,16 +9,51 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage>{
+  double birdYaxis = 0;
+  double time = 0;
+  double height = 0;
+  double intHeight = 0;
+  bool gameStart = false;
+  
+  void jump(){
+    intHeight = birdYaxis;
+  }
+
+  void startGame(){
+    Timer.periodic(Duration(milliseconds: 60), (timer){
+      time += 0.05;
+      height = -4.9 * time * time + 2.8 * time;
+      setState(() {
+        birdYaxis = intHeight - height;
+      });
+      if (birdYaxis > 0){
+
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       body: Column(
         children: [
           Expanded(
-            flex: 2, 
-            child: Container(
-              color: Color.blue,
-              child: Center(child: MyBird()),
+            flex: 2,
+            child: GestureDetector( 
+              onTap: (){
+                if (gameStart){
+                  jump();
+                }
+                else{
+                  startGame();
+                }
+              },
+              child: AnimatedContainer(
+                alignment: Alignment(0, birdYaxis),
+                duration: Duration(milliseconds: 0),
+                color: Colors.blue,
+                child: MyBird(),
+              ),
             ),
           ),
           Expanded(
